092378bbd24a356c16ecf98ac346398e5640214b
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-09 00:18:56 -0300

    prototype development

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 #include <string>
 
 int main(int argc, char const *argv[]) {
-  std::string name = argv[0];
-  auto only_args = name.find("args") > -1;
-  auto only_inputs = name.find("inputs") > -1;
+  auto only_args = strncmp(argv[0], "reargs", 4) == 0;
+  auto only_inputs = strncmp(argv[0], "reinputs", 6) == 0;
   if (!only_inputs) {
     for (int i = 0; i < argc; i++) {
       std::cout << argv[i] << std::endl;
